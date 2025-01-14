fbdee2002e982b0fd270291c7c96654dbab68584
alamin-7 <alamin188914@gmail.com>
2025-01-14 16:45:50 +0600

    gradle project added with file-data-handling sub-project

---
+++ file-data-handling/src/main/java/com/fileDataHandling/FileDataHandling.java
@@ -0,0 +1,13 @@
+package com.fileDataHandling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileDataHandling {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FileDataHandling.class, args);
+        System.out.println("File data handling");
+    }
+}
---
+++ src/test/java/com/handleData/DataHandlingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.handleData;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataHandlingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
